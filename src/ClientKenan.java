acd5fb67e50c1d35cb748935d00379ece303d229
hacker4516 <kenan.tas@elev.ga.ntig.se>
2024-05-17 23:00:52 +0200

    hejdå hejdå hejdå

--- src/ClientKenan.java
+++ src/ClientKenan.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class ClientKenan {
+import java.io.*;
+import java.net.*;
+
+public class ClientKenan {
+    public static void main(String[] args) {
+
+        Socket client = null;
+
+        int portnumber = 47910;
+        if(args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        for( int i = 0; i < 10; i++){
+            try{
+                String msg = "";
+
+                client = new Socket(InetAddress.getLocalHost(), portnumber);
+                System.out.println("Client socket is created " + client);
+
+                OutputStream clientOut = client.getOutputStream();
+                PrintWriter pw = new PrintWriter(clientOut, true);
+
+                InputStream clientIN = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIN));
+
+                BufferedReader stdIN = new BufferedReader(new InputStreamReader(System.in));
+
+
+                System.out.println("Enter your name. Type Bye to exit. ");
+                System.out.print(": ");
+
+                msg = stdIN.readLine().trim();
+                pw.println(msg);
+
+                System.out.println("Message returned from the server = " + br.readLine());
+
+                pw.close();
+                br.close();
+                client.close();
+
+                if (msg.equalsIgnoreCase("bye")){
+                    break;
+                }
+
+
+            } catch (IOException ie){
+                System.out.println("Error is " + ie);
+            }
+        }
+    }
 }
